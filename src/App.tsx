236a1a108166428bcc2dac8e458e9827ab7abdbc
Sabri Shiraz <sshiraz@gmail.com>
2025-11-10 22:17:05 -0800

    Updated tsconfig.tsbuildinfo

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Home from './pages/Home';
-import Upload from './pages/Upload';
 import NotFound from './pages/NotFound';
 
 function App() {
@@ -12,7 +11,6 @@ function App() {
         <Header />
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/upload" element={<Upload />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
         <Footer />
